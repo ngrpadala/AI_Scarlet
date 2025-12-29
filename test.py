49c9c7d1440f84c2f623c4ea672a6adf5bd69d8f
ngrpadala <ngr.padala@gmail.com>
2025-12-29 20:10:07 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from gtts import gTTS
-import os
-
-#text = "హలో, మీరు సరేనా?"
-text = "మీరు ఈరోజు ఎలా ఉన్నారు?"
-tts = gTTS(text, lang="te")
-tts.save("/dev/shm/test_telugu.mp3")
-os.system("mpg123 /dev/shm/test_telugu.mp3")
-
