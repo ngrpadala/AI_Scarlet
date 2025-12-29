578eae4d0f3642767abcdb1fa241c4098bef6f10
ngrpadala <ngr.padala@gmail.com>
2025-12-29 20:09:42 +0530

    Delete test_gtts_telugu.py

--- test_gtts_telugu.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-import os
-
-text = "హాయ్! ఇది స్కార్లెట్ గొంతులో ఒక ప్రేమ కథ ప్రారంభం."
-tts = gTTS(text, lang="te")
-tts.save("/dev/shm/test_telugu.mp3")
-os.system("mpg123 /dev/shm/test_telugu.mp3")
-
